f102fd6c4a6e5d818944ca5895c3c9b8fcfa31cf
liuzekun <liuzekun@iyunxiao.com>
2021-07-20 10:45:40 +0800

    Add:init

---
+++ docs/.enhanceApp/enhanceApp.js
@@ -0,0 +1,6 @@
+import TestUI from "../../src/index.js";
+import "../../src/styles/index.scss";
+
+export default ({ Vue, options, router }) => {
+  Vue.use(TestUI);
+};
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+嘿，快点看我
+<template>
+	<l-button type="primary">Click me</l-button>
+</template>
